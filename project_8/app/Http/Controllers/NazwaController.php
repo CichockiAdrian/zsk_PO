c17cfed234332f46f012894aa16d84c8644fac92
Adrian Cichocki <adrian.cichocki@uczen.zsk.poznan.pl>
2023-12-08 17:40:37 +0100

    Update to project_8

---
+++ project_8/app/Http/Controllers/NazwaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\nazwa;
+use Illuminate\Http\Request;
+
+class NazwaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(nazwa $nazwa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(nazwa $nazwa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, nazwa $nazwa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(nazwa $nazwa)
+    {
+        //
+    }
+}
